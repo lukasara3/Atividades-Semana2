5ee04e44e81517dda81363e57eb9090c87952470
lukasara3 <lucaoamsilva@gmail.com>
2023-10-07 19:02:46 -0300

    Função removerItem

--- service/serviceEstoque.ts
+++ service/serviceEstoque.ts
@@ -1,7 +1,7 @@
 import readCSV from '../model/readCSV'
 import {Data} from '../model/data.interface'
 import writeCSV from '../model/writeCSV'
-import fs from 'fs'
+import fs, { read } from 'fs'
 
 
 const filePath = 'C:\\Users\\lucao\\OneDrive\\Área de Trabalho\\UFMG\\iJunior\\Trainee\\Semana 2\\Atividades-Semana2\\model\\estoque.csv'
@@ -9,7 +9,6 @@ const filePath = 'C:\\Users\\lucao\\OneDrive\\Área de Trabalho\\UFMG\\iJunior\\
 class estoqueService {
 
     async criar(data: Data){
-
         if(!data || data.id == null && data.valor == null){
             throw new Error("Dados inválidos")
         }
@@ -27,15 +26,34 @@ class estoqueService {
             throw new Error("O ID já está sendo utilizado. Favor inserir novo ID.")
         }
         await writeCSV(filePath, [data]);
-
-
-
-
-
-        writeCSV(filePath, [data]);
     }
 
+    async removerItem(id: number){
+        const items = await readCSV(filePath);
+        // Usar .findIndex para achar o item com o ID desejado, que será retornado. Se não for encontrado, -1 será retornado.
+        const itemIndex = items.findIndex((item) => item.id == id)
+
+        if(itemIndex === -1){
+            throw new Error("O ID desejado não foi encontrado no estoque"); // ou "o ID $(id) não foi encontrado"
+        }
+
+        //Remover produto usando .splice para alterar um array - voce vai remover o itemIndex uma vez
+
+        items.splice(itemIndex, 1);
+
+        fs.writeFileSync(filePath, " "); // tirando tudo do arquivo CSV
+        fs.appendFileSync(filePath, "ID,NOME,PESO,VALOR,QUANTIDADE"); // reescrevendo o escopo
+
+        await writeCSV(filePath, items); // reescrevendo o arquivo com a lista atualizada
+    }
+
+
+
+
+
 }
+
+
 
 export default new estoqueService();
 
