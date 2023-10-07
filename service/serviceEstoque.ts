f426cc9ae49aa86cf684b30c42b1b7a85866c301
lukasara3 <lucaoamsilva@gmail.com>
2023-10-07 19:14:58 -0300

    Função valorTotal

--- service/serviceEstoque.ts
+++ service/serviceEstoque.ts
@@ -20,8 +20,8 @@ class estoqueService {
         // Criando uma variavel items e verificar para verificar os id's dos items já presentes no estoque
         // Usando função .find para fazer isso. Se existir o id, lançar erro, senao esperar a função de escrita no CSV.
 
-        const items = await readCSV(filePath);
-        const verificar = items.find(i => i.id == data.id);
+        const itens = await readCSV(filePath);
+        const verificar = itens.find(i => i.id == data.id);
         if(verificar){
             throw new Error("O ID já está sendo utilizado. Favor inserir novo ID.")
         }
@@ -29,9 +29,9 @@ class estoqueService {
     }
 
     async removerItem(id: number){
-        const items = await readCSV(filePath);
+        const itens = await readCSV(filePath);
         // Usar .findIndex para achar o item com o ID desejado, que será retornado. Se não for encontrado, -1 será retornado.
-        const itemIndex = items.findIndex((item) => item.id == id)
+        const itemIndex = itens.findIndex((item) => item.id == id)
 
         if(itemIndex === -1){
             throw new Error("O ID desejado não foi encontrado no estoque"); // ou "o ID $(id) não foi encontrado"
@@ -39,20 +39,33 @@ class estoqueService {
 
         //Remover produto usando .splice para alterar um array - voce vai remover o itemIndex uma vez
 
-        items.splice(itemIndex, 1);
+        itens.splice(itemIndex, 1);
 
         fs.writeFileSync(filePath, " "); // tirando tudo do arquivo CSV
         fs.appendFileSync(filePath, "ID,NOME,PESO,VALOR,QUANTIDADE"); // reescrevendo o escopo
 
-        await writeCSV(filePath, items); // reescrevendo o arquivo com a lista atualizada
+        await writeCSV(filePath, itens); // reescrevendo o arquivo com a lista atualizada
     }
 
-    async listarItems(){
+    async listarItens(){
         const lista = await readCSV(filePath);
         return lista;
     }
 
-    
+    async valorTotal(){
+        const data = await this.listarItens();
+        if(data.length === 0){
+            return 0;
+        }
+        let total = 0; // usando let para poder alterar seu valor ao longo das iteraçoes da funçao
+
+        for (const item of data){
+            console.log(item.valor);
+            if(!isNaN(item.valor)){              // verificando se o item.valor é um número
+                total += +item.valor;
+            }
+        }
+    }
 
 
 
